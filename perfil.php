c80265e861165941d344e43b1d0740d740ca154e
Edilson Junior <etmjr03@gmail.com>
2023-07-30 23:40:03 -0300

    first commit

---
+++ perfil.php
@@ -0,0 +1,46 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Perfil Bruxo</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+  </head>
+  <body>
+    <h1 class="titulo">Olá Bruxo, seja bem-vindo(a)!</h1>
+    
+    <div class="container">
+      <div class="d-flex justify-content-center">
+        <h2 class="">Suas características!</h2>
+      </div>
+      <div class="row d-flex justify-content-center">
+        <div class="form-perfil">
+          <form action="" method="post">
+            <h3 class="text-center">Um pouco mais sobre você:</h3>
+            <div class="row">
+              <img src="" alt="">
+            </div>
+            <div class="row">
+              <label for="" class="text-center">Seu nome bruxo</label>
+              <input type="text" class="mb-3" name="" id="">
+            </div>
+            <div class="row">
+              <label for="" class="text-center">Sua idade</label>
+              <input type="text" class="mb-3" name="" id="">
+            </div>
+            <div class="row">
+              <label for="" class="text-center">Sua magia preferida</label>
+              <input type="text" class="mb-3" name="" id="">
+            </div>
+            <div class="row">
+              <label for="" class="text-center">Seu patrono</label>
+              <input type="text" class="mb-3" name="" id="">
+            </div>              
+          </form>
+        </div>
+      </div>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
+  </body>
+</html>
